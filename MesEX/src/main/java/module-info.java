ea85c697a7facd1e6d0923f00fcfd339df8578aa
dangkhoa-fakemonika <mcfacebook911+github@gmail.com>
2024-11-11 13:47:41 +0700

    Base Project created

---
+++ MesEX/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module org.example.mesex {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.example.mesex to javafx.fxml;
+    exports org.example.mesex;
+}
