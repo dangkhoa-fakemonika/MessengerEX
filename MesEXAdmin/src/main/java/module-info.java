1bc96fd44597d09e03f567f40047172b05702569
dangkhoa-fakemonika <mcfacebook911+github@gmail.com>
2024-11-16 15:20:01 +0700

    Admin Project Configured
    created admin version

---
+++ MesEXAdmin/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module org.example.mesexadmin {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.example.mesexadmin to javafx.fxml;
+    exports org.example.mesexadmin;
+}
