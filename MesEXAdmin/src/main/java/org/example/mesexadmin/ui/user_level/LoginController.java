7dfa2abcd3c2e6cbbf6ceb4e7217f674ec2d1496
dangkhoa-fakemonika <mcfacebook911+github@gmail.com>
2024-12-10 20:43:18 +0700

    Forgot Password UI

--- MesEXAdmin/src/main/java/org/example/mesexadmin/ui/user_level/LoginController.java
+++ MesEXAdmin/src/main/java/org/example/mesexadmin/ui/user_level/LoginController.java
@@ -3,16 +3,17 @@ package org.example.mesexadmin.ui.user_level;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.control.PasswordField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
 
 import org.example.mesexadmin.Main;
+import org.example.mesexadmin.PopUpController;
 import org.example.mesexadmin.SceneManager;
 import org.example.mesexadmin.ui.ControllerWrapper;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class LoginController implements ControllerWrapper {
@@ -22,6 +23,7 @@ public class LoginController implements ControllerWrapper {
     @FXML private Button switchToRegisterButton;
     @FXML private TextField usernameField;
     @FXML private PasswordField passwordField;
+    @FXML private Button forgotAccount;
 
     public void registerScene(ActionEvent actionEvent) throws IOException {
         sceneManager.addScene("Register", "main-register.fxml");
@@ -68,6 +70,25 @@ public class LoginController implements ControllerWrapper {
                     mainScene(actionEvent);
                 } catch (IOException e) {
                     throw new RuntimeException(e);
+                }
+            }
+        });
+
+        forgotAccount.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                try {
+                    FXMLLoader loader = new FXMLLoader(Main.class.getResource("pop-up-forgot-password.fxml"));
+                    Dialog<Objects> dialog = new Dialog<>();
+                    dialog.getDialogPane().getButtonTypes().add(ButtonType.CLOSE);
+                    DialogPane dialogPane = loader.load();
+                    PopUpController popUpController = loader.getController();
+                    popUpController.currentDialog = dialog;
+                    dialog.setDialogPane(dialogPane);
+                    dialog.showAndWait();
+                    dialog.close();
+                } catch (Exception e){
+
                 }
             }
         });
