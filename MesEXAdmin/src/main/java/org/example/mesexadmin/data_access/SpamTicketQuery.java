6cb460a692c616d630623b1a61611485d9ce0818
KanCh <hagiabao1958@gmail.com>
2024-12-11 17:51:59 +0700

    sample join query

--- MesEXAdmin/src/main/java/org/example/mesexadmin/data_access/SpamTicketQuery.java
+++ MesEXAdmin/src/main/java/org/example/mesexadmin/data_access/SpamTicketQuery.java
@@ -1,4 +1,41 @@
 package org.example.mesexadmin.data_access;
 
+import java.util.Arrays;
+
+import org.bson.Document;
+import org.example.mesexadmin.MongoManagement;
+
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Projections;
+
 public class SpamTicketQuery {
+    MongoManagement mongoManagement;
+
+    SpamTicketQuery(MongoManagement mongoManagement) {
+        this.mongoManagement = mongoManagement;
+    }
+
+    public void getSpamTicketDetails() {
+        // Test query
+
+        MongoCollection<Document> tickets = mongoManagement.database.getCollection("spam_ticket");
+
+        AggregateIterable<Document> result = tickets.aggregate(
+
+            Arrays.asList(
+
+                Aggregates.lookup("users", "reporterId", "_id", "reporterDetails"),
+                Aggregates.lookup("users", "reportedUserId", "_id", "reportedUserDetails"),
+
+                Aggregates.project(Projections.fields(
+                    Projections.include("reporterId", "reportedUserId"),
+                    Projections.computed("reporterName", "$reporterDetails.name"),
+                    Projections.computed("reportedUserName", "$reportedUserDetails.name")
+                ))
+            )
+        );
+    }
+ 
 }
