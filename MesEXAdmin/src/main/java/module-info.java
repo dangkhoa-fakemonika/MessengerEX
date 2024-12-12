c310e7d59f5e90e17e56d1e8219881ecbfed1d82
KanCh <hagiabao1958@gmail.com>
2024-12-12 09:36:20 +0700

    update dependency

--- MesEXAdmin/src/main/java/module-info.java
+++ MesEXAdmin/src/main/java/module-info.java
@@ -1,17 +1,17 @@
 module org.example.mesexadmin {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires mongo.java.driver;
     requires javafx.base;
     requires java.naming;
-//    requires javax.mail;
+    requires mongo.java.driver;
+    // requires javafx.web;
+    
+    // requires org.controlsfx.controls;
+    // requires javax.mail;
+    // requires eu.hansolo.tilesfx;
+    // requires org.kordamp.bootstrapfx.core;
+    // requires com.dlsc.formsfx;
+    // requires net.synedra.validatorfx;
 
     exports org.example.mesexadmin;
     exports org.example.mesexadmin.data_class;
